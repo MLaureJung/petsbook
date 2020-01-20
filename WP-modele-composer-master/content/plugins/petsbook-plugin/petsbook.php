663915236ed31cd269570747f5f1d75259054802
romary84 <romary.lambolez@outlook.fr>
2020-01-20 13:37:04 +0100

    ajout plugins v2

---
+++ WP-modele-composer-master/content/plugins/petsbook-plugin/petsbook.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: petsbook plugin
+Description: Mise en place de fonctionnalités pour le thème petsbook
+Author: team petsbook
+Version: 1.0
+*/
+if (!defined('WPINC')) {
+    die;
+}
