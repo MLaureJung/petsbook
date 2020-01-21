35322c4d665201ac7b04d0800c54d0b3ecf0fa0a
romary84 <romary.lambolez@outlook.fr>
2020-01-21 16:08:50 +0100

    new general account ML ok dans php

--- WP-modele-composer-master/content/themes/petsbook/page-account_general.php
+++ WP-modele-composer-master/content/themes/petsbook/page-account_general.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
-<section class="top-info">
+<main class="main">
+    <section class="top-info">
                 <div class="top-info__user">
                     <img src="https://source.unsplash.com/user/jayceexie/aH9Uskj8XTU/200x200" alt="" class="top-info__user__profile">
                     <p class="top-info__user__ID-user">Prenom Nom</p> 
@@ -44,56 +45,70 @@
    </section>
 
    <div class="main-items">
-          <section class="bottom-infos">
-              <div class="animal-picture-profile">
-                <img src="https://source.unsplash.com/user/jayceexie/aH9Uskj8XTU/300x300" alt="" class="icone-animal__pic">
-              </div>
-            </section>
-              <div class="animal-infos">
-                  <label for="animal-infos__title" class="animal-infos__title">INFORMATIONS</label>
+     <div class="account-general-animal-wrapper">
+          <section class="bottom-infos account-general-block" style="background-image: url('https://source.unsplash.com/user/jayceexie/aH9Uskj8XTU/'); background-size:cover; background-position:center center;">
+
+          </section>
+
+              <div class="animal-infos account-general-block">
+                  <label for="animal-infos__title" class="animal-infos__title">
+                    <h3>Informations</h3></label>
                         <ul class="animal-infos__list">
                             <li>Nom</li>
                             <li>Age</li>
                             <li>Sexe</li>
-                            <li>numero de puce</li>
-                            <li>poids</li>
+                            <li>Numero de puce</li>
+                            <li>Poids</li>
                         </ul>
+
+                    <div class="account-general__buttons">
                     <div class="btn-edit">
-                        <a href="#" class="edit">EDITER</a>
+                        <a href="#" class="edit">Editer</a>
                     </div> 
                     <div class="btn-see">
-                      <a href="#" class="see">VOIRE</a>
-                    </div>   
+                      <a href="#" class="see">Voir</a>
+                    </div>  
+                    </div> 
+
                 </div>
         
 
-          <section class="health-book">
-              <label for="health-book__title" class="health-book__title">CARNET DE SANTE</label>
+          <section class="health-book account-general-block">
+              <label for="health-book__title" class="health-book__title">
+                <h3>Carnet de santé</h3></label>
                     <ul>
                         <li>Dernière vaccination</li>
-                        <li>contact vétérinaire</li>
-                        <li>antécedants medicaux</li>
+                        <li>Contact vétérinaire</li>
+                        <li>Antécedants medicaux</li>
                         
                     </ul>
-                    <div class="btn-edit">
-                        <a href="#" class="edit">EDITER</a>
-                    </div> 
-                    <div class="btn-see">
-                      <a href="#" class="see">VOIRE</a>
-                    </div> 
+
+                    <div class="account-general__buttons">
+                      <div class="btn-edit">
+                          <a href="#" class="edit">Editer</a>
+                      </div> 
+                      <div class="btn-see">
+                        <a href="#" class="see">Voir</a>
+                      </div> 
+                    </div>
                     
             </section>
-            <section class="timeline-management">
+
+            <section class="timeline-management account-general-block">
                 <img src="https://source.unsplash.com/user/jayceexie/aH9Uskj8XTU/200x200" alt="" class="timeline-management__pic">
-                <div class="timeline-management__buttons">
+
+              <div class="account-general__buttons">
                 <div class="btn-edit">
-                    <a href="#" class="edit-timeline">EDITER LA TIMELINE</a>
+                    <a href="#" class="edit-timeline">Editer la timeline</a>
                 </div> 
                 <div class="btn-see">
-                  <a href="#" class="see-timeline">VOIRE LA TIMELINE</a>
+                  <a href="#" class="see-timeline">Voir la timeline</a>
                 </div>
               </div> 
-            </section>
-   </div>
 
+            </section>
+          </div>
+   </div>
+   
+  </main>
 <?php get_footer(); ?>
