f67ac654fffb10a64ac21dbbfe92cb7703d107b1
MLaureJung <mlaure.jung@gmail.com>
2020-01-22 10:47:12 +0100

    modif account user info

--- WP-modele-composer-master/content/themes/petsbook/page-account_user_info.php
+++ WP-modele-composer-master/content/themes/petsbook/page-account_user_info.php
@@ -3,21 +3,16 @@
 <section class="top-info">
       <img src="https://source.unsplash.com/user/jayceexie/aH9Uskj8XTU/200x200" alt="" class="top-info__user__profile">
       <p class="top-info__user__ID-user">Prenom Nom</p> 
-      <a href="#" class="top-info__user__edit-profile">Modifier mon profile</a>
+      <a href="#" class="top-info__user__edit-profile">Modifier ma photo de profil</a>
       <div class="add-animal">
-        <div class="add-animal__icone-animal">
-          <img src="https://source.unsplash.com/user/jayceexie/aH9Uskj8XTU/200x200" alt="" class="add-animal__icone-animal__pic__single">
-        </div>
-
-        <a href="#" class="add-animal__add__link"><i class="fa fa-plus" aria-hidden="true"></i>Ajouter un animal</a>
-      </div>
+        
     </div>
    </section>
    
     <div class="main-items">
       <div class="main-items__main-text">
         <div class="main-items__main-text__title">
-          <h1>Informations de l'utilisateur</h1>
+          <h1>Dites quelque chose à propos de vous !</h1>
         </div>
         <section class="user-info">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, sit vero sint voluptatem libero eos dolor, sed tenetur accusamus dolore temporibus odio hic assumenda voluptatum aliquid et! Facere qui aperiam fuga assumenda doloribus corporis, dicta officia aliquid eum voluptatibus amet iste rerum officiis dolores aliquam soluta molestias ipsam? Nisi architecto alias maxime, facilis rem, blanditiis ab autem sapiente eligendi beatae veritatis possimus! Saepe cumque doloremque, harum ut rem repellat reprehenderit totam fuga esse soluta molestiae, excepturi fugiat omnis, temporibus sunt ratione maiores vitae ex tempora iure officia. Exercitationem odit explicabo labore sint, deserunt possimus voluptate amet alias aut hic ipsa minus esse nisi fuga! Optio sit error sint commodi harum sunt dolorem dolorum porro repellendus corporis. Ab deserunt, repellendus recusandae consequuntur id labore aspernatur tempore nemo veniam iusto fugit, necessitatibus nam! Vel saepe libero optio ad? Illum maiores similique voluptatibus vel exercitationem odit officia aliquam non soluta consequuntur, commodi necessitatibus dolor ad amet! Laboriosam, sequi. Reprehenderit dicta aspernatur ad sed qui fugiat possimus, quam facere dignissimos ipsum porro aut eius, odit magnam recusandae nesciunt. Fugit, perferendis? Inventore, quibusdam? Assumenda beatae earum quia accusantium deserunt quidem ea ut, aperiam officiis temporibus esse numquam voluptate atque ullam nostrum? Soluta non cum accusantium ad fugiat. Corporis, illo. Veritatis voluptatum autem voluptates cumque hic cupiditate aspernatur iure quos facilis odit adipisci nobis, mollitia quod praesentium, quidem consequuntur consequatur accusamus neque, obcaecati nulla rem. Tenetur optio ratione laudantium ab quibusdam assumenda deleniti animi hic soluta laborum omnis porro tempore asperiores quos debitis nesciunt nemo itaque, modi reiciendis? Sunt quibusdam voluptatem beatae maiores molestias consequatur ea fuga suscipit amet labore. Nemo corrupti vero esse blanditiis soluta eaque! Consectetur, dolore quo beatae ipsa magni quibusdam repellendus repudiandae magnam non eveniet sit eos quos aperiam provident cumque ratione quae sunt aut accusantium alias optio voluptatum reiciendis illum expedita!</p>
