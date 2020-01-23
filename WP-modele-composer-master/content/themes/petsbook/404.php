2860c3e26aa9fe19833bad5bbc9f9388e503aaf6
miguelalbox <mikyfiestas@gmail.com>
2020-01-23 16:55:59 +0100

    modif pictures 404

--- WP-modele-composer-master/content/themes/petsbook/404.php
+++ WP-modele-composer-master/content/themes/petsbook/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="error">
-      <img class="error__img" src="/images/Illu-2.png" alt="404" >
+      <img class="error__img" src="<?= get_theme_file_uri('public/images/Illu-2.png') ?>" alt="404" >
       <h1 class="error__title">Page non trouvée</h1>
       <p>Vous pouvez <a href="<?= home_url('/') ?>">retourner à l'accueil</a></p>
 
