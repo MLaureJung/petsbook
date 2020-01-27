efc1a45847f8a5cb4a5dfc27147330000a32596a
Aleks512 <aleksandracichocka999@gmail.com>
2020-01-27 13:37:47 +0100

    create dynamic and BO content of privacy mentions

--- WP-modele-composer-master/content/themes/petsbook/page-mentions-legales.php
+++ WP-modele-composer-master/content/themes/petsbook/page-mentions-legales.php
@@ -1,3 +1,32 @@
 <?php get_header(); ?>
 
+
+
+<?php
+ 
+// checks if there are any posts that match the query
+if (have_posts()) :
+ 
+  // If there are posts matching the query then start the loop
+  while ( have_posts() ) : the_post();
+ 
+    // the code between the while loop will be repeated for each post
+    ?>
+    
+    <h2><?php the_title(); ?></h2>;
+ 
+    <p><?php the_content(); ?></p>
+    
+ 
+    <?php
+ 
+    // Stop the loop when all posts are displayed
+ endwhile;
+?>
+
+<?php
+endif;
+?>
+
+
 <?php get_footer(); ?>
