d5a1b43bec0e9cb370a37f1bb34e57308663d7c9
Aleks512 <aleksandracichocka999@gmail.com>
2020-01-27 14:10:00 +0100

    mise en page V1

--- WP-modele-composer-master/content/themes/petsbook/page-mentions-legales.php
+++ WP-modele-composer-master/content/themes/petsbook/page-mentions-legales.php
@@ -8,14 +8,27 @@
 if (have_posts()) :
  
   // If there are posts matching the query then start the loop
-  while ( have_posts() ) : the_post();
+  while ( have_posts() ) : the_post(); ?>
+
+  <div class="info-animal-wrapper"> 
+        <section class="form__pet__infos">
+          <h1 class="form__pet__infos__title"><?php the_title(); ?></h1>
+         
+          <div class="form__pet__infos__edit">
+            <div class="info-glimpse__general-infos">
+              <div class="form__pet__infos__animal">
+                  <h4 class="form__pet__infos__animal__title"><?php the_title(); ?></h4>
+                  <p class="form__pet__infos__animal__user"><?php the_content(); ?></p>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+    </div>
  
     // the code between the while loop will be repeated for each post
-    ?>
-    
-    <h2><?php the_title(); ?></h2>;
- 
-    <p><?php the_content(); ?></p>
+
     
  
     <?php
