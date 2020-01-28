2b03ac37cce29c2e814db16bb454dffee8502eef
sdang <sophie.dang.kieuduyen@gmail.com>
2020-01-28 13:48:53 +0100

    bdd test for fk CONSTRAINT8

--- WP-modele-composer-master/content/themes/petsbook/inc/custom_bdd.php
+++ WP-modele-composer-master/content/themes/petsbook/inc/custom_bdd.php
@@ -17,14 +17,14 @@ function jal_install () {
     dbDelta($sql);
 };
 
-add_action('after_switch_theme','jal_install');
+add_action('after_switch_theme','jal_install',5);
 jal_install();
 
 function jal_version () {
     global $wpdb;
     $table_name = $wpdb->prefix . "newsletter";
 
-    $sql = "ALTER TABLE `$table_name` ADD COLUMN FOREIGN KEY (wp_user_id)
+    $sql = "ALTER TABLE `wp_user_id` ADD COLUMN FOREIGN KEY (wp_user_id)
             ALTER TABLE `$table_name` ADD COLUMN REFERENCES wp_users(ID);";
             
 
@@ -32,8 +32,22 @@ function jal_version () {
     dbDelta($sql);
 };
 
-add_action('after_switch_theme','jal_version');
+add_action('after_switch_theme','jal_version',6);
 jal_version();
+
+function jal_index () {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "newsletter";
+
+    $sql = "CREATE INDEX `wp_user_id` ON `$table_name`;";
+            
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+};
+
+add_action('after_switch_theme','jal_index',7);
+jal_index();
 
 // FOREIGN KEY (ID) 
 // REFERENCES wp_users(ID) 
