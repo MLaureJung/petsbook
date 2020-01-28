75f16a6a0c78dd8789ce1d6241c152e39b9da389
sdang <sophie.dang.kieuduyen@gmail.com>
2020-01-28 13:36:06 +0100

    bdd test for fk CONSTRAINT6

--- WP-modele-composer-master/content/themes/petsbook/inc/custom_bdd.php
+++ WP-modele-composer-master/content/themes/petsbook/inc/custom_bdd.php
@@ -25,7 +25,8 @@ function jal_version () {
     $table_name = $wpdb->prefix . "newsletter";
 
     $sql = "ALTER TABLE `$table_name` ADD COLUMN FOREIGN KEY (wp_user_id)
-            ALTER TABLE `$table_name` ADD COLUMN REFERENCES wp_users(ID);";
+            ALTER TABLE `$table_name` ADD COLUMN REFERENCES wp_users(ID)
+            ALTER TABLE `$table_name` ADD INDEX(`wp_user_id`);";
             
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
